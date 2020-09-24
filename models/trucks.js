e0cafe645dc97f3ca719b3fa6fda9f1b8702a226
Hasheeb Kazi <hasheeb.k@gmail.com>
2020-09-24 14:40:06 -0400

    added trucks js and html

---
+++ models/trucks.js
@@ -0,0 +1,15 @@
+class Truck {
+    constructor(wheeles, engine) {
+        this.wheeles = wheeles;
+        this.engine = engine;
+    }
+
+    get wheeles() {
+        return this.wheeles + " :bb";
+    }
+}
+
+window.onload = () => {
+    const truck1 = new Truck(12, 400);
+    console.log(truck1.wheeles);
+};
