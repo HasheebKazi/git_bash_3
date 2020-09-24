4136dcf5b33e1b4c9624703a106df333bdb0f016
Hasheeb Kazi <hasheeb.k@gmail.com>
2020-09-24 14:50:21 -0400

    added cars model

---
+++ models/cars.js
@@ -0,0 +1,24 @@
+class Car {
+    constructor(wheeles, engine, sunRoof) {
+        this.wheeles = wheeles;
+        this.engine = engine;
+        this.sunRoof = sunRoof;
+    }
+
+    get wheeles() {
+        return this.wheeles + "";
+    }
+
+    get engine() {
+        return this.engine + " :hb";
+    }
+    
+    get sunRoof() {
+        return this.sunRoof ? "Has sun roof." : "Doesn't have sun roof.";
+    }
+}
+
+window.onload = () => {
+    const car1 = new Car(12, 400);
+    console.log(car1.wheeles);
+};
