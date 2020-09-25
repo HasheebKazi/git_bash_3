b3fdfc331c5484d6ea144fa094e00696f02f51ee
Hash <hasheeb.k@gmail.com>
2020-09-24 21:52:05 -0400

    i dont get whats happening

---
+++ models/trucks.js
@@ -0,0 +1,21 @@
+class Truck {
+    constructor(wheeles, engine) {
+        this.wheeles = wheeles;
+        this.engine = engine;
+    }
+
+    get wheeles() {
+        return "I have " + this.wheeles + " wheeles.";
+    }
+
+    set wheeles(wheeles) {
+        if (typeof wheeles === 'number' && wheeles >= 0) {
+            this.wheeles = wheeles;
+        }
+    }
+}
+
+window.onload = () => {
+    const truck1 = new Truck(12, 400);
+    console.log(truck1.wheeles);
+};
