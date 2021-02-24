b824d4804638be66bbc0f645d574b18da8864d01
Hasheeb <hasheeb.k@gmail.com>
2021-02-24 11:55:59 -0500

    added age to cat model

--- models/cat.js
+++ models/cat.js
@@ -5,9 +5,10 @@ class Pet {
 }
 
 class Cat extends Pet {
-    constructor(species, name) {
+    constructor(species, name, age) {
         super(species);
         this.name = name;
+        this.age = age || 0;
     }
 
     get name() {
@@ -17,4 +18,13 @@ class Cat extends Pet {
     set name(name) {
         this.name = name;
     }
+
+    get age() {
+        return this.age + "";
+    }
+
+    set age(age) {
+        this.age = age;
+    }
+
 }
